051ac9c3bdf8b7f4ccc184a3526eae207bbfe046
jrp60 <javirodenas.javirodenas@gmail.com>
2023-02-01 14:59:43 +0100

    Initial Commit

---
+++ innocvExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  innocvExercise
+//
+//  Created by Javier on 1/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
